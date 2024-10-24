3df0f6ef2e1c26d6b99a7d56c33f7db4042affb1
Denny09310 <k.denny2000@gmail.com>
2024-10-24 15:11:31 +0200

    fix: enhanched equality comparison through compiled expressions

--- src/BlazorBindings.Maui/Extensions/EqualityHelper.cs
+++ src/BlazorBindings.Maui/Extensions/EqualityHelper.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Concurrent;
+using System.Linq.Expressions;
 using System.Reflection;
 
 namespace BlazorBindings.Maui.Extensions;
 
 public static class EqualityHelper
 {
-    private static readonly ConcurrentDictionary<(Type, string), FieldInfo> _fieldsRegisty = new();
+    private static readonly ConcurrentDictionary<Key, Func<object, object>> _fieldAccessors = new();
 
     // Added to solve temporarily the issue https://github.com/dotnet/aspnetcore/issues/53361
     public static bool IsEqual(EventCallback callback, object obj)
@@ -15,26 +16,9 @@ public static class EqualityHelper
             return false;
         }
 
-        var delegate1 = GetDelegate(callback);
-        var delegate2 = GetDelegate(other);
-
-        var receiver1 = GetReceiver(callback);
-        var receiver2 = GetReceiver(other);
-
-        return ReferenceEquals(receiver1, receiver2) && Equals(delegate1, delegate2);
-
-        static MulticastDelegate GetDelegate(EventCallback e)
-        {
-            var field = GetField(typeof(EventCallback), "Delegate");
-            return (MulticastDelegate)field.GetValue(e);
-        }
-
-        static IHandleEvent GetReceiver(EventCallback e)
-        {
-            var field = GetField(typeof(EventCallback), "Receiver");
-            return (IHandleEvent)field.GetValue(e);
-        }
+        return AreEqual(callback, other);
     }
+
     public static bool IsEqual<T>(EventCallback<T> callback, object obj)
     {
         if (obj is not EventCallback<T> other)
@@ -42,30 +26,45 @@ public static class EqualityHelper
             return false;
         }
 
-        var delegate1 = GetDelegate(callback);
-        var delegate2 = GetDelegate(other);
-
-        var receiver1 = GetReceiver(callback);
-        var receiver2 = GetReceiver(other);
-
-        return ReferenceEquals(receiver1, receiver2) && Equals(delegate1, delegate2);
-
-
-        static MulticastDelegate GetDelegate(EventCallback<T> e)
-        {
-            var field = GetField(e.GetType(), "Delegate");
-            return (MulticastDelegate)field.GetValue(e);
-        }
-
-        static IHandleEvent GetReceiver(EventCallback<T> e)
-        {
-            var field = GetField(e.GetType(), "Receiver");
-            return (IHandleEvent)field.GetValue(e);
-        }
+        return AreEqual(callback, other);
     }
 
-    private static FieldInfo GetField(Type type, string name) =>
-        _fieldsRegisty.GetOrAdd(
-            key: (type, name),
-            value: type.GetField(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance));
+    private static bool AreEqual(object callback1, object callback2)
+    {
+        var delegate1 = GetDelegate(callback1);
+        var delegate2 = GetDelegate(callback2);
+
+        var receiver1 = GetReceiver(callback1);
+        var receiver2 = GetReceiver(callback2);
+
+        return ReferenceEquals(receiver1, receiver2) && Equals(delegate1, delegate2);
+    }
+
+    private static MulticastDelegate GetDelegate(object callback)
+    {
+        return (MulticastDelegate)GetField(callback.GetType(), "Delegate")(callback);
+    }
+
+    private static Func<object, object> GetField(Type type, string fieldName)
+    {
+        return _fieldAccessors.GetOrAdd(new(type, fieldName), static key =>
+        {
+            var fieldInfo = key.Type.GetField(key.FieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                ?? throw new InvalidOperationException($"Field '{key.FieldName}' not found in type '{key.Type}'.");
+
+            // Create a lambda to access the field without reflection overhead
+            var param = Expression.Parameter(typeof(object), "instance");
+            var fieldAccess = Expression.Field(Expression.Convert(param, key.Type), fieldInfo);
+            var lambda = Expression.Lambda<Func<object, object>>(Expression.Convert(fieldAccess, typeof(object)), param);
+
+            return lambda.Compile();
+        });
+    }
+
+    private static IHandleEvent GetReceiver(object callback)
+    {
+        return (IHandleEvent)GetField(callback.GetType(), "Receiver")(callback);
+    }
+
+    private sealed record Key(Type Type, string FieldName);
 }
